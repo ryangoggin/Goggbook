3b23f2c20c76ac77deaedb879131f216b1609d8a
Ryan Goggin <ryangoggin1@gmail.com>
2023-03-22 18:19:05 -0400

    added update post route for not photo edit

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -34,13 +34,6 @@ def get_all_feed_posts():
         feedUsersLst.append(friend.friend_id)
     feedUsersLst.append(current_user.id)
 
-    # return {
-    #     'friends': [friend.to_dict() for friend in friends],
-    #     'currUser': current_user.to_dict()
-    #     }
-
-
-    # .order_by(Post.created_at.desc())
     posts = Post.query.filter(Post.user_id.in_(feedUsersLst)).order_by(Post.created_at.desc()).all()
     response = [post.to_dict() for post in posts]
 
@@ -62,6 +55,7 @@ def get_post_by_id(id):
     return post.to_dict()
 
 
+# create a new post
 @post_routes.route("", methods=['POST'])
 @login_required
 def create_new_post():
@@ -104,15 +98,71 @@ def create_new_post():
             db.session.commit()
             return {'resPost': new_post.to_dict()}
         # post without a post_pic
-        else:
-            new_post = Post(
-                user_id=res["userId"],
-                content=res["content"],
-                created_at=date.today(),
-                updated_at=date.today()
-            )
+        new_post = Post(
+            user_id=res["userId"],
+            content=res["content"],
+            created_at=date.today(),
+            updated_at=date.today()
+        )
 
-            db.session.add(new_post)
-            db.session.commit()
-            return {'resPost': new_post.to_dict()}
+        db.session.add(new_post)
+        db.session.commit()
+        return {'resPost': new_post.to_dict()}
     return jsonify({"errors": form.errors}), 400
+
+
+# edit a post
+@post_routes.route("/<int:id>", methods=['PUT'])
+@login_required
+def edit_post(id):
+    '''
+    create a new post and return it as a dictionary if successful
+    '''
+    post = Post.query.get(id)
+
+    if post is None:
+        return jsonify({'error': 'Post not found'}), 404
+
+    if post.user_id != current_user.id:
+         return jsonify({'error': 'Cannot edit posts that are not your\'s'}), 403
+
+    res = request.get_json()
+
+    form = PostForm()
+    form['csrf_token'].data = request.cookies["csrf_token"]
+
+    errors = {}
+
+    if len(res["content"]) > 2000:
+            errors["content"] = "Messages must be less than 2000 characters"
+            return jsonify({"errors": errors}), 400
+
+    if form.validate_on_submit():
+
+        # # post with a post_pic
+        # if form.data['post_pic']:
+        #     image = form.data['post_pic']
+
+        #     image.filename = get_unique_filename(image.filename)
+
+        #     upload = upload_file_to_s3(image)
+
+        #     if "url" not in upload:
+        #         return jsonify("error uploading image"), 400
+
+            # content=res["content"]
+            # post_pic=upload["url"]
+            # updated_at=date.today()
+
+        #     db.session.commit()
+        #     return {'resPost': post.to_dict()}
+        # post without a post_pic
+        post.content=res["content"]
+        post.updated_at=date.today()
+
+        db.session.commit()
+        return {'resPost': post.to_dict()}
+    return jsonify({"errors": form.errors}), 400
+
+
+# delete a post
