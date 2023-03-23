42e0a0e83485c5192384c71be9737502fa63b783
Ryan Goggin <ryangoggin1@gmail.com>
2023-03-23 12:10:02 -0400

    added friend slice of state to get friend details on feed

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify
-from flask_login import login_required
-from app.models import User
+from flask_login import login_required, current_user
+from app.models import User, Friend
 
 user_routes = Blueprint('users', __name__)
 
@@ -23,3 +23,20 @@ def user(id):
     """
     user = User.query.get(id)
     return user.to_dict()
+
+@user_routes.route('/friends')
+@login_required
+def user_friends():
+    """
+    Query for a user's friends by user id and return their user info as a dictionary
+    """
+    friends = Friend.query.filter(Friend.user_id == current_user.id)
+    userFriendLst = []
+    for friend in friends:
+        userFriendLst.append(friend.friend_id)
+    userFriendLst.append(current_user.id)
+
+    friendUsers = User.query.filter(User.id.in_(userFriendLst)).all()
+    response = [user.to_dict() for user in friendUsers]
+
+    return {'friends': response}
