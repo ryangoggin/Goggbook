d2c53d9ace405e520b037973ddc07029eb232a20
Ryan Goggin <ryangoggin1@gmail.com>
2023-03-25 13:00:31 -0400

    improved feed styling

--- react-app/src/components/CommentItem/index.js
+++ react-app/src/components/CommentItem/index.js
@@ -37,6 +37,30 @@ function CommentItem({ comment }) {
 
     if (!commentUser) return null;
 
+    // helper to make updateAt a time since
+    function convertUpdatedAt(updatedAt) {
+        const updatedAtDate = new Date(updatedAt);
+        const now = new Date();
+        const timeDiffMs = now.getTime() - updatedAtDate.getTime();
+
+        // Convert time difference from milliseconds to minutes, hours, days, and years
+        const timeDiffMin = Math.floor(timeDiffMs / 60000);
+        const timeDiffHr = Math.floor(timeDiffMin / 60);
+        const timeDiffDay = Math.floor(timeDiffHr / 24);
+        const timeDiffYr = Math.floor(timeDiffHr / 365);
+
+        // Return formatted time string
+        if (timeDiffYr > 0) {
+            return `${timeDiffYr}y`;
+        } else if (timeDiffDay > 0) {
+            return `${timeDiffDay}d`;
+        } else if (timeDiffHr > 0) {
+            return `${timeDiffHr}h`;
+        } else {
+            return `${timeDiffMin}m`;
+        }
+    }
+
     return (
         <div className='comment-item'>
             <div className='comment-left'>
@@ -46,7 +70,7 @@ function CommentItem({ comment }) {
                         <p className='comment-fullname'>{commentUser.firstName} {commentUser.lastName}</p>
                         <p className='comment-content'>{comment.content}</p>
                     </div>
-                    <p className='comment-updated-at'>{comment.updatedAt}</p>
+                    <p className='comment-updated-at'>{convertUpdatedAt(comment.updatedAt)}</p>
                 </div>
             </div>
             <div className='comment-right'>
@@ -54,13 +78,13 @@ function CommentItem({ comment }) {
                     <>
                         <OpenModalButton
                             className="edit-button"
-                            buttonText='Edit'
+                            buttonText={<i className="fa-solid fa-pencil"></i>}
                             onItemClick={closeMenu}
                             modalComponent={<EditCommentModal comment={comment} />}
                         />
                         <OpenModalButton
                             className="delete-button"
-                            buttonText='Delete'
+                            buttonText={<i className="fa-regular fa-trash-can"></i>}
                             onItemClick={closeMenu}
                             modalComponent={<DeleteCommentModal comment={comment} />}
                         />
