ed1e33f9e56499db340ca6a89e15d8c3339c0e05
Ryan Goggin <ryangoggin1@gmail.com>
2023-03-30 20:02:41 -0400

    made about links on left feed

--- react-app/src/components/Feed/index.js
+++ react-app/src/components/Feed/index.js
@@ -58,6 +58,22 @@ function Feed() {
         history.push(`/users/${sessionUser.id}`);
     };
 
+    function handleLinkedIn() {
+        window.open("https://www.linkedin.com/in/ryangoggin20/", "_blank");
+    }
+
+    function handleGithub() {
+        window.open("https://github.com/ryangoggin", "_blank");
+    }
+
+    function handleGoggInn() {
+        window.open("https://ryangoggin-gogginn.onrender.com/", "_blank");
+    }
+
+    function handlePixelPal() {
+        window.open("https://pixelpal.onrender.com/", "_blank");
+    }
+
     const closeMenu = () => setShowMenu(false);
 
     let friendsArr = [];
@@ -86,6 +102,36 @@ function Feed() {
                     <button className="user-profile-button" onClick={handleCurrProfileClick}>
                         <img className='profile-pic' src={`${sessionUser.profilePic}`} alt={`${sessionUser.firstName} ${sessionUser.lastName} Profile`} />
                         <p className="profile-fullname">{sessionUser.firstName} {sessionUser.lastName}</p>
+                    </button>
+                </div>
+                <div className="link-button-container">
+                    <button className="user-profile-button" onClick={handleLinkedIn}>
+                        <div className="linkedin-container">
+                            <i className="fa-brands fa-linkedin"></i>
+                        </div>
+                        <p className="profile-fullname">Ryan's LinkedIn</p>
+                    </button>
+                </div>
+                <div className="link-button-container">
+                    <button className="user-profile-button" onClick={handleGithub}>
+                        <div className="github-container">
+                            <i className="fa-brands fa-github"></i>
+                        </div>
+                        <p className="profile-fullname">Ryan's Github</p>
+                    </button>
+                </div>
+                <div className="link-button-container">
+                    <button className="user-profile-button" onClick={handleGoggInn}>
+                        <div className="gogginn-container">
+                            <img className="gogginn-logo" src="https://goggbook-aws.s3.amazonaws.com/mug-hot-solid.png" alt="GoggInn Logo" />
+                        </div>
+                        <p className="profile-fullname">GoggInn</p>
+                    </button>
+                </div>
+                <div className="link-button-container">
+                    <button className="user-profile-button" onClick={handlePixelPal}>
+                        <img className="pixel-pal-logo" src="https://goggbook-aws.s3.amazonaws.com/pixel-pal-logo.png" />
+                        <p className="profile-fullname">PixelPal</p>
                     </button>
                 </div>
             </div>
