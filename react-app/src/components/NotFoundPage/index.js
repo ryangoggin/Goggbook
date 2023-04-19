1ac8ef1fb84851d69a15f1da14bd6bdcc052e017
Ryan Goggin <ryangoggin1@gmail.com>
2023-04-19 18:37:27 -0400

    updated 404 page

---
+++ react-app/src/components/NotFoundPage/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import './NotFoundPage.css';
+
+function NotFoundPage() {
+    const history = useHistory();
+
+    const returnHome = async (e) => {
+        e.preventDefault();
+        history.push("/");
+    }
+
+    return (
+        <>
+            <div className='not-found-container'>
+                <img className='not-found-image' src="https://goggbook-aws.s3.amazonaws.com/facebook-404.svg" alt="not found" />
+                <h3 className="page-not-found">Page Not Found</h3>
+                <div className='when-this-happens-container'>
+                    <p className='when-this-happens-text'>
+                        When this happens, it's usually because you went to a
+                        page that doesn't exist. Please click the button below
+                        to go back to Goggbook.
+                    </p>
+                </div>
+                <button className="return-button" onClick={returnHome}>
+                    <p className="return-text">
+                        Return to Goggbook
+                    </p>
+                </button>
+            </div>
+        </>
+    );
+}
+
+export default NotFoundPage;
